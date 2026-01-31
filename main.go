651269c09022670a8c51945c323ac68b121748e9
Kovalev Maksim <teasec4@yandex.ru>
2026-01-31 18:05:50 +0800

    linked list

--- main.go
+++ main.go
@@ -1,97 +1,66 @@
 package main
 
-import (
-	"cmp"
+import ( 
 	"fmt"
 )
 
-type OrderableFunc[T any] func(t1, t2 T) int
 
 // Node
 type Node[T any] struct{
 	val T
-	left, right *Node[T]
+	next *Node[T]
 }
 
-type Tree[T any] struct{
-	f OrderableFunc[T]
-	root *Node[T]
+type List[T any] struct{
+	Head *Node[T]
+	Tail *Node[T]
+	Len int 
 }
 
-func NewTree[T any](f OrderableFunc[T]) *Tree[T]{
-	return &Tree[T]{
-		f: f,
+func (l *List[T]) Add(v T){
+	newNode := &Node[T]{val:v}
+	
+	if l.Head == nil{
+		l.Head = newNode
+		l.Tail = newNode
+	} else {
+		l.Tail.next = newNode
+		l.Tail = newNode
 	}
+	
+	l.Len++
 }
 
-func (t *Tree[T]) Add(v T){
-	t.root = t.root.Add(t.f, v)
+func (l *List[T]) Get(index int) (T, bool){
+	var zero T
+	
+	if index < 0 || index >=l.Len {
+		return zero, false
+	}
+	
+	cur := l.Head
+	for i := 0; i < index; i++{
+		cur = cur.next
+	}
+	
+	return cur.val, true
 }
 
-func (t *Tree[T]) Contains(v T) bool{
-	return t.root.Contains(t.f, v)
-}
-
-func (n *Node[T]) Add(f OrderableFunc[T], v T) *Node[T]{
-	if n == nil{
-		return &Node[T]{val: v}
+func (l *List[T]) Print() {
+	for n := l.Head; n != nil; n = n.next {
+		fmt.Println(n.val)
 	}
-	switch r := f(v, n.val);{
-		case r <= -1:
-		n.left = n.left.Add(f, v)
-		case r >= 1:
-		n.right = n.right.Add(f, v)
-	}
-	return n
-}
-
-func (n *Node[T]) Contains(f OrderableFunc[T], v T) bool{
-	if n == nil{
-		return false
-	}
-	switch r:= f(v, n.val);{
-		case r<= -1:
-		return n.left.Contains(f, v)
-		case r >= 1:
-		return n.right.Contains(f, v)
-	}
-	return true
 }
 
 func main() {
-	t1 := NewTree(cmp.Compare[int])
-	t1.Add(10)
-	t1.Add(15)
-	t1.Add(8)
+	var list List[string]
+
+	list.Add("a")
+	list.Add("b")
+	list.Add("c")
+
+	list.Print()
 	
-	fmt.Println(t1.Contains(15))
-	fmt.Println(t1.Contains(20))
-	
-	v1 := 1
-	v2 := 2.5
-	v1 = doubleValue(v1)
-	v2 = doubleValue(v2)
-	fmt.Println(v1)
-	fmt.Println(v2)
-	
-}
-
-
-type CustomNumber interface{
-	~int | ~int8 | ~int16 | ~int32 | ~int64 |
-	~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 |
-	~float32 | ~float64
-}
-
-func doubleValue[T CustomNumber](value T) T{
-	return value * 2
-}
-
-type Printable interface{
-	fmt.Stringer
-	~int | ~float64
-}
-
-func PrintValue[T Printable](v T) {
-	fmt.Println(v.String())
+	s, _ := list.Get(1)
+	fmt.Println(s)
 }
