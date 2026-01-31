ec89511d7017e9295735e596c84cad9e9ca367f6
Kovalev Maksim <teasec4@yandex.ru>
2026-01-31 16:35:00 +0800

    generic ex

--- main.go
+++ main.go
@@ -66,5 +66,32 @@ func main() {
 	
 	fmt.Println(t1.Contains(15))
 	fmt.Println(t1.Contains(20))
+	
+	v1 := 1
+	v2 := 2.5
+	v1 = doubleValue(v1)
+	v2 = doubleValue(v2)
+	fmt.Println(v1)
+	fmt.Println(v2)
+	
 }
 
+
+type CustomNumber interface{
+	~int | ~int8 | ~int16 | ~int32 | ~int64 |
+	~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 |
+	~float32 | ~float64
+}
+
+func doubleValue[T CustomNumber](value T) T{
+	return value * 2
+}
+
+type Printable interface{
+	fmt.Stringer
+	~int | ~float64
+}
+
+func PrintValue[T Printable](v T) {
+	fmt.Println(v.String())
+}
